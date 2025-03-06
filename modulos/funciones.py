a811b770b9a42c53cbbf9283f8138506c75073ed
KeinnerL <194520454+KeinnerL@users.noreply.github.com>
2025-03-06 12:03:25 -0500

    actualizacion funcion agregar

--- modulos/funciones.py
+++ modulos/funciones.py
@@ -1,7 +1,6 @@
 import json
 
 def agregarJoya(ruta,caso):
-    try:
         with open (ruta,"r",encoding="utf-8") as file:
             biblioteca=json.load(file)
             match caso:
@@ -10,8 +9,29 @@ def agregarJoya(ruta,caso):
 =========================================
       |Quilate de el diamante|
 =========================================
+1.0.50 ct
+2.1.00 ct
+3.1.50 ct  
+4.2.00 ct                  
 Por favor ingrese quilate de el diamante.                         
 """)
+                    try:
+                        casoC=input(int("->"))
+                        match casoC:
+                            case 1:
+                                quilate=0.50
+                            case 2:
+                                quilate=1.00
+                            case 3:
+                                quilate=1.50
+                            case 4:
+                                quilate=2.00
+                            case _:
+                                print("La eleccion ingresada no existe....")
+                                x=input("Presione enter para continuar...")
+                    except ValueError:
+                        print("Por favor ingrese una eleccion valida...")
+                        x=input("Presione enter para continuar.")
                     quilate=input(float(""))
                     print("""
 ========================================================
@@ -25,18 +45,18 @@ Por favor ingrese la calidad del corte de el diamante.
 5.Pobre                             
 """)
                     try:
-                        casoC=input(int(""))
+                        casoC=input(int("->"))
                         match casoC:
                             case 1:
-                                pass
+                                corte=1.20
                             case 2:
-                                pass
+                                corte=1.10
                             case 3:
-                                pass
+                                corte=1.00
                             case 4:
-                                pass
+                                corte=0.90
                             case 5:
-                                pass
+                                corte=0.80
                             case _:
                                 print("La eleccion ingresada no existe....")
                                 x=input("Presione enter para continuar.")
@@ -48,25 +68,25 @@ Por favor ingrese la calidad del corte de el diamante.
                 |Color|
 ==========================================
 Por favor ingrese el color del diamante.
-1.Incoloro
-2.Casi incoloro
-3.Tono ligero
-4.Marron/Amarillo
+1.D-F (Incoloro)
+2.G-J (Casi incoloro)
+3.K-M (Tono ligero)
+4.N-Z (Amarillo/Marrón)
 5.Fantasia(Azul, Rosa, Verde, Amarillo intenso, Rojo.)                         
 """)
                     try:
                         casoCL=input(int(""))
                         match casoCL:
                             case 1:
-                                pass
+                                color="d-f"
                             case 2:
-                                pass
+                                color="g-j"
                             case 3:
-                                pass
+                                color="k-m"
                             case 4:
-                                pass
+                                color="n-z"
                             case 5:
-                                pass
+                                color="fan"
                             case _:
                                 print("La eleccion ingresada no existe....")
                                 x=input("Presione enter para continuar.")
@@ -88,22 +108,23 @@ Por favor ingrese la claridad del diamante.
                             casoCla=input(int(""))
                             match casoCla:
                                 case 1:
-                                    pass
+                                    claridad=1.50
                                 case 2:
-                                    pass
+                                    claridad=1.30
                                 case 3:
-                                    pass
+                                    claridad=1.15
                                 case 4:
-                                    pass
+                                    claridad=1.00
                                 case 5:
-                                    pass
+                                    claridad=0.75
                                 case _:
                                     print("La eleccion ingresada no existe....")
                                     x=input("Presione enter para continuar.")
                         except ValueError:
                             print("Por favor ingrese una eleccion valida...")
                             x=input("Presione enter para continuar.")
-    except Exception as e :
+                        precioBQ=0
+                        if quilate==0.20 and 
         print("error al agregar informacion de joya...")
         x=input ("presione en ter para continuar")
         return agregarJoya()
